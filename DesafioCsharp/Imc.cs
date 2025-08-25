4fa4c60b9326f36e0df68c8503bec57d7c7f82af
PeterSteve <pedropote677@gmail.com>
2025-08-24 23:47:20 -0300

    Update Imc.cs
    Atualizando o ultimo commit que faltava

--- DesafioCsharp/Imc.cs
+++ DesafioCsharp/Imc.cs
@@ -1,1 +1,26 @@
+using System;
+
+class Imc
+{
+    static void Main()
+    {
+        Console.Write("Peso (kg): ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Altura (m): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+        Console.WriteLine($"IMC: {imc:F2}");
+
+        if (imc < 18.5)
+            Console.WriteLine("Abaixo do peso");
+        else if (imc < 25)
+            Console.WriteLine("Peso normal");
+        else if (imc < 30)
+            Console.WriteLine("Sobrepeso");
+        else
+            Console.WriteLine("Obesidade");
+    }
+}
 ﻿
