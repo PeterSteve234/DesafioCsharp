1d584225e000dc7ae17f0c007263e1d4a6b459a2
PeterSteve <pedropote677@gmail.com>
2025-08-24 23:45:12 -0300

    Update Calculadora.cs

--- DesafioCsharp/Calculadora.cs
+++ DesafioCsharp/Calculadora.cs
@@ -1,47 +1,41 @@
-﻿using System;
+using System;
 
-class Imc
-
+class Calculadora
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.WriteLine("Calculadora de IMC");
 
-        Console.Write("Digite seu peso (kg): ");
+        
+        Console.Write("Digite seu peso em kg: ");
         double peso = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Digite sua altura (m): ");
+        
+        Console.Write("Digite sua altura em metros (ex: 1.75): ");
         double altura = Convert.ToDouble(Console.ReadLine());
 
-        double imc = CalcularIMC(peso, altura);
+        
+        double imc = peso / (altura * altura);
 
+        
         Console.WriteLine($"Seu IMC é: {imc:F2}");
-        ClassificarIMC(imc);
-    }
 
-    static double CalcularIMC(double peso, double altura)
-    {
-        if (altura == 0)
-        {
-            Console.WriteLine("Erro: altura não pode ser zero.");
-            return 0;
-        }
-        return peso / (altura * altura);
-    }
-
-    static void ClassificarIMC(double imc)
-    {
+        // Classificação
         if (imc < 18.5)
-            Console.WriteLine("Classificação: Abaixo do peso");
-        else if (imc < 25)
-            Console.WriteLine("Classificação: Peso normal");
-        else if (imc < 30)
-            Console.WriteLine("Classificação: Sobrepeso");
-        else if (imc < 35)
-            Console.WriteLine("Classificação: Obesidade grau I");
-        else if (imc < 40)
-            Console.WriteLine("Classificação: Obesidade grau II");
+        {
+            Console.WriteLine("Abaixo do peso");
+        }
+        else if (imc >= 18.5 && imc < 25)
+        {
+            Console.WriteLine("Peso normal");
+        }
+        else if (imc >= 25 && imc < 30)
+        {
+            Console.WriteLine("Sobrepeso");
+        }
         else
-            Console.WriteLine("Classificação: Obesidade grau III (mórbida)");
+        {
+            Console.WriteLine("Obesidade");
+        }
     }
 }
